f0828d4a05dacd3ea8acf287b7c506b38cf02cd3
Miguel Angel Luna <miguelangel.lunaarmada@soprahr.com>
2018-09-25 12:52:16 +0200

    Added intl provider and browser router

--- react-front/src/index.js
+++ react-front/src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { IntlProvider } from 'react-intl';
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <IntlProvider locale="en">
+      <App />
+    </IntlProvider>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
 registerServiceWorker();
