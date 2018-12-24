d2bc8c36cb1c8134075456eda34f44187f64f1e8
Miguel Angel Luna <miguelangel.lunaarmada@soprahr.com>
2018-12-24 12:38:28 +0100

    Extracted front

--- react-front/src/services/Search.js
+++
@@ -1,9 +0,0 @@
-// GENERAL FUNCTIONALITY
-import axios from "axios";
-import config from "../config";
-import {getDefaultHeaders} from "./Auth";
-
-export const searchBrandsOrVehicles = (typedMessage, type) => {
-    const headers = getDefaultHeaders();
-    return axios.get(`${config.REST_BASE_URL}/search?name=${typedMessage}?type=${type}`, {headers: headers});
-};
